011a021c7798f0e6ad1c8a9c0c66a23ac1d7dffc
lautarohaili <lautarohaili@gmail.com>
2022-05-26 12:01:37 -0300

    probando carrito nuevo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Cart from "./componentes/Cart";
 import ItemDetailContainer from "./componentes/containers/ItemDetailContainer";
 import ItemListContainer from "./componentes/containers/ItemListContainer";
-import CartContextProv from "./context/CartContext";
-import NavBar from "./componentes/navBar/NavBar";
+import NuevoNavBar from "./componentes/navBar/NuevoNavBar";
+import CartPage from "./pages/cartPage";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
@@ -11,12 +12,12 @@ function App() {
     <CartContextProv>
       <BrowserRouter>
         <div className="App">
-          <NavBar />
+          <NuevoNavBar />
           <Routes>
             <Route path="/" element={<ItemListContainer />} />
             <Route path="/category/:id" element={<ItemListContainer />} />
             <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
-            <Route path="/cart" element={<Cart />} />
+            <Route path="/cart" element={<CartPage />} />
             <Route path="/*" element={<Navigate to="/" replace />} />
           </Routes>
         </div>
